e37f952ab27ba879444d7f3f7321df5dfe158ccc
Roslyn Saplicki <roslynsaplicki@roslyns-mbp.home>
2018-09-16 13:17:06 -0700

    High-level pseudocode.

---
+++ js/game.js
@@ -0,0 +1,18 @@
+
+
+
+// Start Game
+    // initGame
+    // Choose moves
+// Click Tiles
+    // Tiles can't flip once selected
+        // Image change
+    // Know when they have won or lost
+        // Alert
+    // Combinations
+        // Lots of little functions
+// Restart
+    // Reset Button Functionality
+    // Bring back to the Original initGame function
+
+// Keep Score
