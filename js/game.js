16c6944145f0e0cf6581f7a7009347a35c7115e5
Roslyn Saplicki <roslynsaplicki@roslyns-mbp.home>
2018-09-16 23:00:22 -0700

    Added some game logic.

--- js/game.js
+++ js/game.js
@@ -1,31 +1,70 @@
-/*----- constants -----*/
 
 
-/*----- app's state (variables) -----*/
-// var that manipulate the state of the game
 
+
+
+
+
+
+// function endGame (win){
+//     if (checkWin(user1) === true) {
+//         scoreX += 1;
+//         $('.scoreX').html(scoreX);
+//         alert("X won the game!");		    	
+//         resetBoard();
+
+//     };
+//     if (checkWin(user2) === true) {
+//         scoreY += 1;
+//         $('.scoreY').html(scoreY);
+//         alert("Y won the game!");		    	
+//         resetBoard();
+//     };
+//     if (checkTie() == true) {
+//         alert("Y won the game!");
+//         resetBoard();
+//     };
+//     }
+
+//     
+
+
+
+
+    /*----- constants -----*/
+var board;
+
+    /*----- app's state (variables) -----*/
+
+    // var that manipulate the state of the game
 var tilesFlipped;
 var tilesUnflipped;
 var gameOver;
+var x;
+var o;
+var user1;
+var user2;
+var playerTurn = "user1";
+var letter = "img/flipped_x_tile.png" || "img/flipped_o_tile.png";
+ 
+    /*----- cached element references -----*/
 
-/*----- cached element references -----*/
-// vars manipulated for appearance, the selectors we would put in our css
-
+    // vars manipulated for appearance, the selectors we would put in our css
 var resetBtn;
 var score;
 var tileArray; // This is what I am comparing to the wireArray imaging.
 
-/*----- event listeners -----*/
-
+    /*----- event listeners -----*/
+ 
 function flipTile(event) {
     if (!tilesUnflipped[event.target.id] && !gameOver) {
         //change image
-        event.target.src = "img/flipped_x_tile.png"
+        event.target.src = "img/flipped_x_tile.png";
+    } else if ( user2 && !tilesUnflipped[event.target.id] && !gameOver) {
+        event.target.src = "img/flipped_o_tile.png";
     }
 }
-
-
-function reset(event){
+ function reset(event){
     gameOver = false;
     //show all unflipped tiles
     for (let i = 0; i < tileArray.length; i++) {
@@ -33,11 +72,15 @@ function reset(event){
       initGame();
     }
 }
+    /*----- functions -----*/
 
-
-
-/*----- functions -----*/
-function initGame() {
+ function initGame() {
+        score = {
+            "X": 0,
+            "O": 0
+        };
+        moves = 0;
+        turn = "X";
     tilesFlipped = [];
     tilesUnflipped = {
       tile1: false,
@@ -52,7 +95,7 @@ function initGame() {
     }
 }
 
-function endGame (win){
+ function endGame (win){
   
     gameOver = true;
   
@@ -63,28 +106,25 @@ function endGame (win){
     }
   }
 
-
-
-
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM loaded");
     body = document.body;
     
-    tile1Image = document.getElementById('tile1');
-    tile2Image = document.getElementById('tile2');
-    tile3Image = document.getElementById('tile3');
-    tile4Image = document.getElementById('tile4');
-    tile5Image = document.getElementById('tile5');
-    tile6Image = document.getElementById('tile6');
-    tile7Image = document.getElementById('tile7');
-    tile8Image = document.getElementById('tile8');
-    tile9Image = document.getElementById('tile9');
+    tile1Image = document.getElementById('1');
+    tile2Image = document.getElementById('2');
+    tile3Image = document.getElementById('3');
+    tile4Image = document.getElementById('4');
+    tile5Image = document.getElementById('5');
+    tile6Image = document.getElementById('6');
+    tile7Image = document.getElementById('7');
+    tile8Image = document.getElementById('8');
+    tile9Image = document.getElementById('9');
     
   
     tileArray = document.getElementById("gameTiles").children;
   
     resetBtn = document.querySelector('button');
-    wireBox = document.getElementById('wireBox');
+    gameTiles = document.getElementById('gameTiles');
     
     gameTiles.addEventListener('click', flipTile);
     
@@ -93,18 +133,65 @@ function endGame (win){
   });
   
 
-// Start Game
+
+ // Start Game
     // initGame
     // Choose moves
+// Click Tiles
 // During Game/Click Tiles
     // Tiles can't flip once selected
         // Image change
     // Know when they have won or lost
-        // Alert
-    // Combinations
-        // Lots of little functions
-// Restart
-    // Reset Button Functionality
-    // Bring back to the Original initGame function
 
-// Keep Score
+
+
+    /* function checkWin () {
+
+        var tile1Image = document.getElementById('1');
+            tile2Image = document.getElementById('2');
+            tile3Image = document.getElementById('3');
+            tile4Image = document.getElementById('4');
+            tile5Image = document.getElementById('5');
+            tile6Image = document.getElementById('6');
+            tile7Image = document.getElementById('7');
+            tile8Image = document.getElementById('8');
+            tile9Image = document.getElementById('9');
+    
+        if (tile1Image.innerHTML !== "" && tile1Image.innerHTML === tile2Image.innerHTML && tile1Image.innerHTML === tile3Image.innerHTML) {
+            console.log('win');
+        } else if (tile4Image.innerHTML !== "" && tile4Image.innerHTML === tile5Image.innerHTML && tile4Image.innerHTML === tile6Image.innerHTML) {
+            console.log('win');
+        } else if (tile7Image.innerHTML !== ""  && tile7Image.innerHTML === tile8Image.innerHTML && tile7Image.innerHTML === tile9Image.innerHTML) {
+            console.log('win');
+        } else if (tile1Image.innerHTML !== ""  && tile1Image.innerHTML === tile4Image.innerHTML && tile1Image.innerHTML === tile7Image.innerHTML) {
+            console.log('win');
+        } else if (tile2Image.innerHTML !== ""  && tile2Image.innerHTML === tile5Image.innerHTML && tile2Image.innerHTML === tile8Image.innerHTML) {
+            console.log('win');
+        } else if (tile3Image.innerHTML !== ""  && tile3Image.innerHTML === tile6Image.innerHTML && tile3Image.innerHTML === tile9Image.innerHTML) {
+            console.log('win');
+        } else if (tile1Image.innerHTML !== ""  && tile1Image.innerHTML === tile5Image.innerHTML && tile1Image.innerHTML === tile9Image.innerHTML) {
+            console.log('win');
+        } else if (tile3Image.innerHTML !== ""  && tile3Image.innerHTML === tile5Image.innerHTML && tile3Image.innerHTML === tile7Image.innerHTML) {
+            console.log('win');
+        } else {
+            console.log("cat's game");
+        }
+    }
+    
+    var gameTiles = document.querySelectorAll(".gameTiles"), x_or_o = 0;
+    
+    for(var i =0; i < gameTiles.length - 1; i++) {
+        tiles[i].onclick = function() {
+            if(x_or_o%2 === 0){
+                console.log(x_or_o);
+                this.innerHTML = "x";
+                checkWin();
+                x_or_o  += 1;
+            } else {
+                console.log(x_or_o);
+                this.src = "o";
+                checkWin();
+                x_or_o  += 1;
+            }
+        };
+    } */
